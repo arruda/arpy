0b2a9f2b5df9848069e446db88f828bdf2bcc622
Felipe Arruda Pontes <felipe.pontes@corp.globo.com>
2013-02-25 13:50:41 -0300

    Another fix

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ import serial
 from flask import Flask, render_template,request
 
 app = Flask(__name__)
-
-ser = serial.Serial('/dev/ttyACM0',9600)
-
+try:
+    ser = serial.Serial('/dev/ttyACM0',9600)
+except:
+    ser = lambda:True
+    ser.write = lambda x: x
 
 @app.route('/',methods=['POST', 'GET'])
 def command():
     cmd = None
     if request.method == 'POST':
-        msg = request.form['cmd']
+        cmd = request.form['cmd']
         ser.write(cmd)
     return render_template('cmd.html', cmd=cmd)
 
